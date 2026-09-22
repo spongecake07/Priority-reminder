8ba3287cf07726666b2225f85b8c16da7b4c6d96
spongecake07 <raoulgombar@gmail.com>
2026-09-22 15:17:32 -0400

    Force v10 app refresh

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='priority-reminders-v9';
-const ASSETS=['./','./index.html','./styles.css','./nav-polish.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='priority-reminders-v10';
+const ASSETS=['./','./index.html','./styles.css','./nav-polish.css','./app.js?v=10','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
