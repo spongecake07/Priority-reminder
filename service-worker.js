7a5a8864488e19bd5bee96d0869dadf9361b8b1d
spongecake07 <raoulgombar@gmail.com>
2026-09-22 15:12:35 -0400

    Refresh cache after JavaScript fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='priority-reminders-v7';
+const CACHE='priority-reminders-v8';
 const ASSETS=['./','./index.html','./styles.css','./nav-polish.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
