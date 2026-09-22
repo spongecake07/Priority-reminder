7a658f2156af88ad614019d7a91d02212683542e
spongecake07 <raoulgombar@gmail.com>
2026-09-22 17:03:56 -0400

    Refresh cache for persistent navigation

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='priority-reminders-v22';
-const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=22','./app.js?v=22','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='priority-reminders-v23';
+const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=23','./app.js?v=23','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
