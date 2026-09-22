464632e05fa6b65e4751751e867049640537ce26
spongecake07 <raoulgombar@gmail.com>
2026-09-22 17:37:09 -0400

    Refresh cache for cleaner home

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='priority-reminders-v24';
-const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=24','./app.js?v=24','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='priority-reminders-v25';
+const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=25','./app.js?v=25','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
