a4fe7eea6c8e324e7055cf73917c8e66a2e7a196
spongecake07 <raoulgombar@gmail.com>
2026-09-22 17:43:24 -0400

    Refresh cache for Minna branding

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='priority-reminders-v25';
-const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=25','./app.js?v=25','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='minna-v26';
+const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=26','./app.js?v=26','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
