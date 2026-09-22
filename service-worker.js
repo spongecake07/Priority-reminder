f2425671ba27f7958bb9280ee24af2d8e9e5fd2f
spongecake07 <raoulgombar@gmail.com>
2026-09-22 16:22:32 -0400

    Force v18 reminder composer refresh

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='priority-reminders-v17';
-const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=17','./app.js?v=17','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='priority-reminders-v18';
+const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=18','./app.js?v=18','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
