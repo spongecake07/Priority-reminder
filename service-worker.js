301dc6da1faf4622ebe148d6dfb99d190b6fe75e
spongecake07 <raoulgombar@gmail.com>
2026-09-23 11:32:43 -0400

    Refresh cache for personalized home v51

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='minna-v50';
-const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=50','./app.js?v=50','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='minna-v51';
+const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=51','./app.js?v=51','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
