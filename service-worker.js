fc70cc803134e33b72e30af28ef43d7c5f447795
spongecake07 <raoulgombar@gmail.com>
2026-09-23 16:49:47 -0400

    Refresh PWA cache for v63

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='minna-v62';
-const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=62','./app.js?v=62','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='minna-v63';
+const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=63','./app.js?v=63','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
