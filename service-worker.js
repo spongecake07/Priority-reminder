49bcba682ecc0b339a4cb7fa789f09b1233abf1e
spongecake07 <raoulgombar@gmail.com>
2026-09-23 11:59:18 -0400

    Refresh cache for palette previews v54

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='minna-v53';
-const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=53','./app.js?v=53','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='minna-v54';
+const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=54','./app.js?v=54','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
