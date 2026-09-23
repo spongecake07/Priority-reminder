bf200f07d68e5a89e0b5d0b1290165e79bbde70f
spongecake07 <raoulgombar@gmail.com>
2026-09-23 08:07:45 -0400

    Refresh cache for completed reminders

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='minna-v33';
-const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=33','./app.js?v=33','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='minna-v34';
+const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=34','./app.js?v=34','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
