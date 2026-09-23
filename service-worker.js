82be197419dc0cb0cf6abe3a8415f997e6f2c042
spongecake07 <raoulgombar@gmail.com>
2026-09-23 11:55:58 -0400

    Refresh cache for detailed medals v53

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='minna-v52';
-const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=52','./app.js?v=52','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='minna-v53';
+const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=53','./app.js?v=53','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
