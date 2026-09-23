0a50ca6cfac97e89c91b5b7fdf682921389886fc
spongecake07 <raoulgombar@gmail.com>
2026-09-23 10:12:36 -0400

    Refresh cache for Calendar alignment repair

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='minna-v42';
-const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=42','./app.js?v=42','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='minna-v43';
+const ASSETS=['./','./index.html','./styles.css','./nav-polish.css?v=43','./app.js?v=43','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
